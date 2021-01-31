e081cb148770b97ca0f6660fe8f0c4f098302ccb
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2021-01-31 20:14:59 +0530

    Create MissionEncypt.py

---
+++ Python/MissionEncypt.py
@@ -0,0 +1,17 @@
+n=input()
+p=list(n)
+# i=-2
+k=''
+h=''
+for i in range(0,len(p),2):
+    k=k+p[i]
+# txt = k[::-1]
+for j in range(1,len(p),2):
+    h=h+p[j]
+//print(h)
+txt = k[::-1]
+print(txt+h)
+
+//QuitTheMission
+
+//osieTiQuthMsin
