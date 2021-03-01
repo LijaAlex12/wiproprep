0aa8eabb12a0ef65e2605da148b678b942e8d622
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2021-03-01 14:37:21 +0530

    Update basics.c

--- C/basics.c
+++ C/basics.c
@@ -1,6 +1,10 @@
+int temp=0;
 for (i=0;i<n-1;i++){
  for (j=i+1;j<n;j++){
+  
    if (arr[i]>arr[j]){
-        swap
+        temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
         
         }
